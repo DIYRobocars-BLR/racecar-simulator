0b71d49a12eed1eff889d0b01212ddc3281fe9ac
Vivek Chand <vivekchand19@gmail.com>
2018-09-15 16:00:47 +0530

    update mover

--- racecar_control/scripts/mover.py
+++ racecar_control/scripts/mover.py
@@ -1,51 +1,72 @@
 #!/usr/bin/env python
 
+import cv2
+import numpy
 import rospy
-
 from ackermann_msgs.msg import AckermannDriveStamped
-from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image, PointCloud2, CameraInfo
 
 
-def rgb_callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "RGB: I heard %s", data.data)
+class Mover(object):
 
+    def rgb_callback(self, data):
+        bridge = CvBridge()
+        cv_image = bridge.imgmsg_to_cv2(data, 'rgb8')
+        boundaries = [
+            ([255, 2, 2], [255, 2, 2])
+        ]
+        for (lower, upper) in boundaries:
+            # create NumPy arrays from the boundaries
+            lower = numpy.array(lower, dtype='uint8')
+            upper = numpy.array(upper, dtype='uint8')
 
-def point_cloud_callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "PointCloud: I heard %s", data.data)
+            # find the colors within the specified boundaries and create a mask out of it
+            mask = cv2.inRange(cv_image, lower, upper)
+            # apply the mask to the image
+            output = cv2.bitwise_and(cv_image, cv_image, mask=mask)
 
+            masked_image_msg = bridge.cv2_to_imgmsg(output, 'rgb8')
+            self.segmented_img_pub.publish(masked_image_msg)
 
-def depth_callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "Depth: I heard %s", data.data)
+        rospy.loginfo(rospy.get_caller_id() + "RGB: I heard %s", cv_image)
 
+    def point_cloud_callback(self, data):
+        pass
 
-def mover():
-    speed = 0.5
-    turn = 0.25
+    def depth_callback(self, data):
+        pass
 
-    pub = rospy.Publisher('/vesc/high_level/ackermann_cmd_mux/input/nav_0', AckermannDriveStamped, queue_size=10)
-    rospy.Subscriber("/camera/zed/color/image_raw", Image, rgb_callback)
-    rospy.Subscriber("/camera/zed/depth_registered/points", Image, point_cloud_callback)
-    rospy.Subscriber("/camera/zed/depth/camera_info", Image, depth_callback)
-    rospy.init_node('mover', anonymous=False)
-    rate = rospy.Rate(10)  # 10hz
-    while not rospy.is_shutdown():
-        msg = AckermannDriveStamped()
+    def __init__(self):
+        speed = 0.5
+        turn = 0.25
 
-        x = 1
-        th = 0.5
+        self.pub = rospy.Publisher('/vesc/high_level/ackermann_cmd_mux/input/nav_0', AckermannDriveStamped,
+                                   queue_size=10)
+        self.segmented_img_pub = rospy.Publisher("/mover/segmented_image", Image, queue_size=10)
+        rospy.Subscriber("/camera/zed/color/image_raw", Image, self.rgb_callback)
+        rospy.Subscriber("/camera/zed/depth_registered/points", PointCloud2, self.point_cloud_callback)
+        rospy.Subscriber("/camera/zed/depth/camera_info", CameraInfo, self.depth_callback)
+        rospy.init_node('mover', anonymous=False)
+        rate = rospy.Rate(10)  # 10hz
+        while not rospy.is_shutdown():
+            msg = AckermannDriveStamped()
 
-        msg.header.stamp = rospy.Time.now()
-        msg.header.frame_id = ''
-        msg.drive.speed = x * speed
-        msg.drive.steering_angle = th * turn
+            x = 1
+            th = 0.5
 
-        rospy.loginfo(msg)
-        pub.publish(msg)
-        rate.sleep()
+            msg.header.stamp = rospy.Time.now()
+            msg.header.frame_id = ''
+            msg.drive.speed = x * speed
+            msg.drive.steering_angle = th * turn
+
+            rospy.loginfo(msg)
+            self.pub.publish(msg)
+            rate.sleep()
 
 
 if __name__ == '__main__':
     try:
-        mover()
+        Mover()
     except rospy.ROSInterruptException:
         pass
